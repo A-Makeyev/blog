151902a7a3fb32c9970994fa64a8375979e5e836
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2024-01-20 00:45:22 +0200

    added Post, Tag, templates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from app.models import Post
 
-# Create your views here.
+def index(request):
+    context = { 'posts': Post.objects.all() }
+    return render(request, 'app/index.html', context)
+
+def post_page(request, slug):
+    context = { 'post': Post.objects.get(slug=slug) }
+    return render(request, 'app/post.html', context)
