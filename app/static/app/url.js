97a91861ac4398f824cf2c56b09b14f4f3b6afaf
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2024-01-26 11:53:56 +0200

    update url.js

--- app/static/app/url.js
+++ app/static/app/url.js
@@ -1,19 +1,19 @@
-let header = document.querySelector("header");
-let link = document.querySelector(".links");
-let main = document.querySelector("main");
-main.style.paddingTop = header.offsetHeight + "px";
+let header = document.querySelector('header')
+let link = document.querySelector('.links')
+let main = document.querySelector('main')
+main.style.paddingTop = header.offsetHeight + 'px'
 
 function menu() {
-  link.classList.toggle("active");
+  link.classList.toggle('active')
 }
 
 function toggleDiv(a) {
   a = a.parentNode.parentNode.parentNode
   replybox = a.querySelector('.comment-box')
-  if (replybox.style.display === "none") {
-    replybox.style.display = "block";
+  if (replybox.style.display === 'none') {
+    replybox.style.display = 'block'
   } else {
-    replybox.style.display = "none";
+    replybox.style.display = 'none'
   }
 }
 
@@ -23,8 +23,6 @@ function doScrolling(element, duration) {
   // If element is close to page's bottom then window will scroll only to some position above the element.
   var targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY
   var diff = targetY - startingY
-  // Easing function: easeInOutCubic
-  // From: https://gist.github.com/gre/1650294
   var easing = function (t) { return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1 }
   var start
 
@@ -37,10 +35,8 @@ function doScrolling(element, duration) {
     var time = timestamp - start
     // Get percent of completion in range [0, 1].
     var percent = Math.min(time / duration, 1)
-    // Apply the easing.
-    // It can cause bad-looking slow frames in browser performance tool, so be careful.
+    // Apply the easing. It can cause bad-looking slow frames in browser performance tool, so be careful.
     percent = easing(percent)
-
     window.scrollTo(0, startingY + diff * percent)
 
     // Proceed with animation as long as we wanted it to.
@@ -51,7 +47,5 @@ function doScrolling(element, duration) {
 }
   
 // Apply event handlers. Example of firing the scrolling mechanism.
-document.getElementById('scrollMid').addEventListener('click', doScrolling.bind(null, '#middle', 1000))
-
-// Or simply:
-doScrolling('#reply-20', 2000)
+// document.getElementById('#').addEventListener('click', doScrolling.bind(null, '#middle', 1000))
+// doScrolling('#', 100)
